657971ceac5acd499b9a7614de285e3f6321fc1f
sooch-h <haso3838@colorado.edu>
2025-09-09 09:22:47 -0600

    Add files via upload

--- EulerAngles313.m
+++ EulerAngles313.m
@@ -1,8 +1,8 @@
 function attitude313 = EulerAngles313(DCM)
 
-Y = arctan2d((DCM(3,1))/(-1*DCM(3,2)));
-B = arccos(DCM(3,3));
-A = arctan2d((DCM(1,3))/(-1*DCM(2,3)));
+Y = atan2((DCM(3,1)),(-1*DCM(3,2)));
+B = acos(DCM(3,3));
+A = atan2((DCM(1,3)),(-1*DCM(2,3)));
 
 attitude313 = [A;B;Y];
 end
