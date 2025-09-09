0373ea09d3b84a0243d91223386651aa95e606d9
grap7936 <grap7936@colorado.edu>
2025-09-09 09:48:27 -0600

    EulerAngles321.m

--- EulerAngles321.m
+++ EulerAngles321.m
@@ -1,6 +1,6 @@
 function attitude321 = EulerAngles321(DCM)
 
-% Function Purpose: Use an input DCM to output euler angles in desired parameters for use in
+% Purpose: Use an input DCM to output euler angles in desired parameters for use in
 % analysis of a aircraft system.
 
 % Inputs:
@@ -11,14 +11,19 @@ function attitude321 = EulerAngles321(DCM)
 % • attitude321: 3 x 1 vector with the 3-2-1 Euler angles in the form attitude321 = [𝛼𝛼, 𝛽𝛽, 𝛾𝛾]T (In
 % ASEN 3728 notation, this would be equivalent to [𝜙,𝜃,𝜓]T ).
 
+attitude321 = zeros(3, 1, length(DCM));
+
+for i = 1 : length(DCM)
 
 % Use definitions of angles from DCM elements to attain each euler angle
-phi = arctan2d( (DCM(3,1))/(-1*DCM(3,2)) );
-theta = -1*arcsin(DCM(1,3));
-psi = arctan2d( (DCM(1,2))/(DCM(1,1)) );
+phi = atan2(DCM(2,3,i),DCM(3,3,i));
+theta = -1*asin(DCM(1,3,i));
+psi = atan2( (DCM(1,2,i)),(DCM(1,1,i)) );
 
 % Define attitude in terms of extracted euler angles
-attitude321 = [phi, theta, psi]';
+attitude321(:,:,i) = [phi, theta, psi]';
+
+end
 
 
 end
