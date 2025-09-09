fadbedaf4ec55a66ca713826a016e0dfa1a3cc3a
grap7936 <grap7936@colorado.edu>
2025-09-09 09:48:07 -0600

    EulerAngles313.m

--- EulerAngles313.m
+++ EulerAngles313.m
@@ -1,8 +1,15 @@
 function attitude313 = EulerAngles313(DCM)
 
-Y = atan2((DCM(3,1)),(-1*DCM(3,2)));
-B = acos(DCM(3,3));
-A = atan2((DCM(1,3)),(-1*DCM(2,3)));
+attitude313 = zeros(3,1,length(DCM));
 
-attitude313 = [A;B;Y];
+for i = 1 : length(DCM)
+
+Y = atan2((DCM(3,1,i)),(-1*DCM(3,2,i)));
+B = acos(DCM(3,3,i));
+A = atan2((DCM(1,3,i)),(DCM(2,3,i)));
+
+attitude313(:,:, i) = [A;B;Y];
+
+end
+
 end
